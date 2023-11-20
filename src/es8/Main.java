dfba72a58d13b3df525490463df6a8149c652d41
udhin98 <udhin1998@gmail.com>
2023-11-20 21:57:44 +0100

    es8

--- src/es8/Main.java
+++ src/es8/Main.java
@@ -4,16 +4,27 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-    HashSet<Oggetto> hashSet = createHashSet();
+    HashSet<String> hashSet = createHashSet();
+
+    HashSet<String> nuovoHashSet = new HashSet<>(hashSet);
+    nuovoHashSet.add("4");
+    String nuovoValore = "7";
+
+    if(nuovoHashSet.contains(nuovoValore)) {
+        System.out.println("Seguente valore fa parte: " + nuovoValore);
+    } else {
+        System.out.println("Seguente valore non fa parte: " + nuovoValore);
+    }
+    System.out.println(nuovoHashSet);
     }
 
-    public static HashSet<Oggetto> createHashSet () {
+    public static HashSet<String> createHashSet () {
 
-        HashSet<Oggetto> hashSet = new HashSet<String>();
+        HashSet<String> hashSet = new HashSet<>();
 
-        Oggetto newOggetto = new Oggetto(1, "Mario");
-
-        hashSet.add(newOggetto);
+        hashSet.add("1");
+        hashSet.add("2");
+        hashSet.add("3");
 
        return hashSet;
     }
