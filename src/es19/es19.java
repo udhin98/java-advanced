cc814fc12707b9777114336e4cda9171d026f6d0
udhin98 <udhin1998@gmail.com>
2023-12-01 15:09:41 +0100

    es20

--- src/es19/es19.java
+++ src/es19/es19.java
@@ -11,8 +11,15 @@ import java.time.format.DateTimeFormatter;
 //        Stampa il risultato localizzata per l'Italia
 public class es19 {
     public static void main(String[] args) {
-        String data = "2023-03-01T13:00:00Z";
-        LocalDateTime dateTime = LocalDateTime.parse(data, DateTimeFormatter.ISO_DATE_TIME);
+        //String data = "2023-03-01T13:00:00Z";
+       OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+       System.out.println(data);
+       OffsetDateTime newData = data
+               .plusYears(1)
+               .minusMonths(1)
+               .plusDays(7);
+        System.out.println(newData);
+
 
 
 
