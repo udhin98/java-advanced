988a073eda16a9b9a93d7e033a86541c861b12d3
udhin98 <udhin1998@gmail.com>
2023-12-03 21:52:11 +0100

    es20

--- src/es19/es19.java
+++ src/es19/es19.java
@@ -3,6 +3,7 @@ package es19;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 //Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
 //        aggiungi un anno
@@ -13,12 +14,12 @@ public class es19 {
     public static void main(String[] args) {
         //String data = "2023-03-01T13:00:00Z";
        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-       System.out.println(data);
        OffsetDateTime newData = data
                .plusYears(1)
                .minusMonths(1)
                .plusDays(7);
-        System.out.println(newData);
+       String dataItalia = newData.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy", Locale.ITALY));
+        System.out.println(dataItalia);
 
 
 
