aca8d4f9512be77fb4e1cc03cfe903a7d274bc6a
udhin98 <udhin1998@gmail.com>
2023-12-10 22:22:50 +0100

    es29

--- src/es29/es29.java
+++ src/es29/es29.java
@@ -1,11 +1,12 @@
 package es29;
+
 //Crea un'interfaccia Shape e un metodo draw, crea 2 o più classi che implementino Shape.
 //        Cre una enum che abbia tutti i tipi di shape che hai creato ( cerchio, rettangolo etc etc)
 //        Crea una factory class che abbia un metodo che ritorni la forma corretta (shape) in base all'enum in input.
 //        Stampa a video il risultato della funzione draw di ogni shape creata
 public class es29 {
     public static void main(String[] args) {
-    Shape shapeCerchio = Factory.nuovoTipo(Tipi.CERCHIO);
+        Shape shapeCerchio = Factory.nuovoTipo(Tipi.CERCHIO);
         shapeCerchio.draw();
 
         Shape shapeRettangolo = Factory.nuovoTipo(Tipi.RETTANGOLO);
